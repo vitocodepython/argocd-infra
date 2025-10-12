4ace5cc61acc7986ca529aae6a01366990eeb115
vitocodepython <vitoactility@gmail.com>
2025-10-12 21:24:42 +0200

    Preparation

---
+++ Vagrantfile
@@ -0,0 +1,12 @@
+Vagrant.configure("2") do |config|
+  config.vm.box = "hashicorp-education/ubuntu-24-04"
+  config.vm.box_version = "0.1.0"
+
+  config.vm.define "cicd" do |cicd|
+    cicd.vm.hostname = "cicd"
+    cicd.vm.network "private_network", ip: "192.168.56.110"
+
+    # Exécution automatique du script setup.sh à la création
+    cicd.vm.provision "shell", path: "scripts/setup.sh"
+  end
+end
---
+++ scripts/setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+export DEBIAN_FRONTEND=noninteractive
+set -x
+
+echo "Préparation du système"
+sudo apt-get update -y
+sudo apt-mark hold grub-efi-amd64 grub-efi-amd64-bin grub2-common linux-image-generic linux-headers-generic linux-firmware openssh-server cloud-init snapd
+
+echo "Installation des dépendances"
+sudo apt-get install -yq curl git vim net-tools apt-transport-https ca-certificates gnupg lsb-release
+
+echo "Installation de Docker"
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+sudo usermod -aG docker vagrant
+sudo systemctl enable docker
+sudo systemctl start docker
+
+echo "Installation de kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+echo "Installation de k3d"
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+k3d version || echo " Erreur lors de l'installation de k3d"
+
+echo "Installation et configuration d'ArgoCD"
+k3d cluster create argocd-cluster --api-port 6550 -p "8080:80@loadbalancer" -p "8888:8888@loadbalancer" --agents 1
+
+kubectl create namespace argocd || true
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+echo " Attente de la disponibilité du serveur ArgoCD"
+kubectl wait --for=condition=available --timeout=600s deployment/argocd-server -n argocd
+
+# Récupération du mot de passe admin ArgoCD
+ARGO_PWD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" 2>/dev/null | base64 -d 2>/dev/null || echo "inconnu")
+
+# Exposition automatique d'ArgoCD sur le port 9090
+nohup kubectl port-forward svc/argocd-server -n argocd --address 0.0.0.0 9090:80 > /dev/null 2>&1 &
+
+echo " [SETUP TERMINÉ]"
+echo " ArgoCD accessible à : http://192.168.56.110:9090"
+echo " Identifiant : admin"
+echo " Mot de passe : $ARGO_PWD"
